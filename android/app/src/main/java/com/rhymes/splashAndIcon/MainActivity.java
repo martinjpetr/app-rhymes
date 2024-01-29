47cea49c7a7516d89b5b8e43e7ad7c7948f062a8
Martin Petr <m_petr@utb.cz>
2024-01-29 05:45:14 +0100

    Přidání ikony a splash screenu

---
+++ android/app/src/main/java/com/rhymes/splashAndIcon/MainActivity.java
@@ -0,0 +1,5 @@
+package com.rhymes.splashAndIcon;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,27 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'App Rhymes',
+  appId: 'com.rhymes.splashAndIcon',
+  appName: 'app-rhymes',
   webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      //backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      //androidSpinnerStyle: "large",
+      //iosSpinnerStyle: "small",
+      //spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      //layoutName: "launch_screen",
+      //useDialog: true,
+    },
+  },
   server: {
     androidScheme: 'https'
   }
